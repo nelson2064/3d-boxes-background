863f94b9cb576be88425cd53e42b427470bf2777
Nelson Parajuli <121667396+nelson2064@users.noreply.github.com>
2023-12-06 12:22:31 +0545

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,152 @@ function createBoxes() {
 }
 
 createBoxes()
+
+
+
+
+
+
+
+
+// const boxesContainer = document.getElementById('boxes')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+// function createBoxes() {
+//   for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//       const box = document.createElement('div')
+//       box.classList.add('box')
+//       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+//       boxesContainer.appendChild(box)
+//     }
+//   }
+// }
+
+// createBoxes()
+
+// const boxesContainer = document.getElementById('boxes')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+// function createBoxes() {
+//   for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//       const box = document.createElement('div')
+//       box.classList.add('box')
+//       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+//       boxesContainer.appendChild(box)
+//     }
+//   }
+// }
+
+// createBoxes()
+
+// const boxesContainer = document.getElementById('boxes')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+// function createBoxes() {
+//   for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//       const box = document.createElement('div')
+//       box.classList.add('box')
+//       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+//       boxesContainer.appendChild(box)
+//     }
+//   }
+// }
+
+// createBoxes()
+// const boxesContainer = document.getElementById('boxes')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+// function createBoxes() {
+//   for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//       const box = document.createElement('div')
+//       box.classList.add('box')
+//       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+//       boxesContainer.appendChild(box)
+//     }
+//   }
+// }
+
+// createBoxes()
+
+// const boxesContainer = document.getElementById('boxes')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+// function createBoxes() {
+//   for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//       const box = document.createElement('div')
+//       box.classList.add('box')
+//       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+//       boxesContainer.appendChild(box)
+//     }
+//   }
+// }
+
+// createBoxes()
+
+// const boxesContainer = document.getElementById('boxes')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+// function createBoxes() {
+//   for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//       const box = document.createElement('div')
+//       box.classList.add('box')
+//       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+//       boxesContainer.appendChild(box)
+//     }
+//   }
+// }
+
+// createBoxes()
+
+// const boxesContainer = document.getElementById('boxes')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+// function createBoxes() {
+//   for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//       const box = document.createElement('div')
+//       box.classList.add('box')
+//       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+//       boxesContainer.appendChild(box)
+//     }
+//   }
+// }
+
+// createBoxes()
+// const boxesContainer = document.getElementById('boxes')
+// const btn = document.getElementById('btn')
+
+// btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
+
+// function createBoxes() {
+//   for (let i = 0; i < 4; i++) {
+//     for (let j = 0; j < 4; j++) {
+//       const box = document.createElement('div')
+//       box.classList.add('box')
+//       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+//       boxesContainer.appendChild(box)
+//     }
+//   }
+// }
+
+// createBoxes()
